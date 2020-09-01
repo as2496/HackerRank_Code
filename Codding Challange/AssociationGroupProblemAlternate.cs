129ab97821fadafc7b3d9474bba1b60414b88870
Anushree Saha <SahaA1@mskcc.org>
2020-09-01 15:42:18 -0400

    TWo new coding challange from big4

---
+++ Codding Challange/AssociationGroupProblemAlternate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codding_Challange
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AssociationGroupProblemAlternate
+    {
+        //public static void Main(String[] args)
+        //{
+        //    var list = new List<List<string>>
+        //    {
+        //        new List<string>
+        //        {
+        //            "item1","item2"
+        //        },
+        //        new List<string>
+        //        {
+        //            "item2","item1"
+        //        },
+        //         new List<string>
+        //        {
+        //            "item3","item4"
+        //        }
+        //    };
+
+
+        //    GetAssociatedGruops(list);
+        //}
+
+        private static List<string> GetAssociatedGruops(List<List<string>> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var listChanged = false;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if(list[i].Intersect(list[j]).Any())
+                    {
+                        var unionList = list[i].Union(list[j]).ToList();
+                        unionList.Sort();
+                        list.Add(unionList);
+                        list.Remove(list[i]);
+                        list.Remove(list[j-1]);
+                        listChanged = true;
+                    }
+                }
+                i = (listChanged) ? i - 1 : i;
+            }
+
+            if(list.Count > 1)
+            {
+                var dist = new Dictionary<List<string>, int>();
+                foreach(var item in list)
+                {
+                    dist.Add(item, item.Count);
+                }
+                dist.OrderByDescending(x => x.Value);
+            }
+           var result = (list.Count > 1)? list.OrderByDescending(x => x.Count).Take(1).ToList(): list.ToList();
+            return  result.First();
+        }
+    }
+}
---
+++ Codding Challange/RoboticGame.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codding_Challange
+{
+    public class RoboticGame
+    {
+        /// <summary>
+        /// Return an integer total score after going through steps of action
+        /// digit -> the current score is that digit and Total score is summation of current score & previous total score
+        /// Z ->previous and total score is reset as if privious throw never happened
+        /// X -> current score is 2*previous score,Total score is summation of current score & previous total score 
+        /// + ->current score is summation of two previous current score, Total score is summation of current score & previous total score
+        /// </summary>
+        /// <param name="inputList"></param>
+        /// <returns></returns>
+        public static int GetTotalScore(List<string> blocks)
+        {
+            var currentScore = 0;
+            var previousScore = 0;
+            var previousCurrent = 0;
+            var TotalScore = 0;
+            var prePrevious = 0;
+            foreach (var item in blocks)
+            {
+                bool isDigit = int.TryParse(item, out int result);
+                if (isDigit == true)
+                {
+                    previousScore = TotalScore;
+                    previousCurrent = currentScore;
+                    currentScore = result;
+                    TotalScore = TotalScore + currentScore;
+                }
+                else if (item.Equals("Z", StringComparison.OrdinalIgnoreCase))
+                {
+                    TotalScore = previousScore;
+                    currentScore = previousCurrent;
+                }
+                else if (item.Equals("X", StringComparison.OrdinalIgnoreCase))
+                {
+                    previousCurrent = currentScore;
+                    currentScore = currentScore * 2;
+                    previousScore = TotalScore;
+                    TotalScore = TotalScore + currentScore;
+                }
+                else if (item.Equals("+"))
+                {
+                    prePrevious = previousCurrent;
+                    previousCurrent = currentScore;
+                    previousScore = TotalScore;
+                    currentScore = prePrevious + previousCurrent;
+                    TotalScore = currentScore + TotalScore;
+                }
+            }
+            
+            return TotalScore;
+        }
+        public static void Main(String[] args)
+        {
+            var list = new List<string>
+            {
+                "4","1","2","+","z" //1// exp3
+                //"5","-2","4","Z","X","9","+","+"
+            };
+
+
+
+            GetTotalScore(list);
+        }
+    }
+}
